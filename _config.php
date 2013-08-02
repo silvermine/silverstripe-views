5e56ac0c8a7bc3af84fed62219e204185a307890
Jeremy Thomerson <jeremy@thomersonfamily.com>
2013-08-02 12:48:30 +0000

    Fix RSS feeds and remove hacks to use them

--- _config.php
+++ _config.php
@@ -11,15 +11,11 @@
  * This configuration file adds the ViewHost extension to all SiteTree nodes to
  * make this a "plug-and-play" module.  You simply drop it in your SilverStripe
  * web root and it will be enabled on your SiteTree.
- *
- * NOTE: this module requires a fork of ajshort's silverstripe-itemsetfield
- * module available at https://github.com/jthomerson/silverstripe-itemsetfield
- * This is because there are additional features in jthomerson's itemsetfield
- * that have not yet been merged to ajshort's original version.
  */
 
 // TODO: review all uses of the _t() function in this module
-
+// TODO: convert this to YML config and add README.md as documentation rather
+// than just comments here.
 DataObject::add_extension('SiteTree', 'ViewHost');
 DataObject::add_extension('SiteConfig', 'ViewHost');
 
@@ -27,7 +23,11 @@ DataObject::add_extension('SiteConfig', 'ViewHost');
  * If you want to enable the RSS functionality for views, you can add this
  * extension as shown here, or to customize the functionality you can create
  * your own subclass of RSSContentControllerExtension and override functions
- * within it.
+ * such as createFeed($view, $controller, $response) within it.
  * Object::add_extension('ContentController', 'RSSContentControllerExtension');
+ *
+ * To enable this you will also need to add the 'rss' action to the
+ * allowed_actions array in your Page_Controller class (or on specific pages'
+ * controllers if you only want it available on those page types).
  */
 
--- code/RSSContentControllerExtension.php
+++ code/RSSContentControllerExtension.php
@@ -12,6 +12,8 @@
  */
 class RSSContentControllerExtension extends Extension {
 
+   private static $allowed_actions = array('rss');
+
    /**
     * Create the RSS feed for a given view.  This function is designed to be
     * overridden by any client code that wants to customize the behavior of the
@@ -19,50 +21,49 @@ class RSSContentControllerExtension extends Extension {
     *
     * @param View &$view the view to create a feed from
     * @param ContentController the controller that owns this extension and is currently responding to this request
+    * @param the SS_HTTPResponse that will be returned with the feed
     * @return RSSFeed the RSS feed for the view
     */
-   protected function createFeed(&$view, $controller) {
+   protected function createFeed(&$view, $controller, $response) {
       $items = $view->Results();
       return new RSSFeed($items, $controller->request->getURL(), _t('Views.' . $view->Name . 'RSSTitle'));
    }
 
    /**
-    * Handles a request that is for a view's RSS feed.
+    * This is registered as an allowed_action on controllers that have the
+    * extension. SS ContentController will call this directly to allow
+    * processing of the request.
+    *
+    * The URL pattern that will invoke us it: $URLSegment/$Action/$ID/$OtherID
+    *
+    * - URLSegment is the URLSegment of the page that is hosting the feed
+    * - Action is "rss"
+    * - ID is the view name that we will use to look up the view on the page
+    * - OtherID is "feed" from our "feed.xml" extension in our links
     */
-   public function interceptRequest($controller, $viewName) {
+   public function rss() {
+      $controller = $this->owner;
       $page = Director::get_current_page();
-      $view = $page && $page->hasExtension('ViewHost') ?
+      $response = $controller->getResponse() ?: new SS_HTTPResponse();
+
+      $params = $controller->request->allParams();
+      $viewName = isset($params['ID']) ?: null;
+      $view = $page && $viewName && $page->hasExtension('ViewHost') ?
          $page->GetView($viewName) :
          false;
 
-      $response = Controller::curr()->getResponse();
-      if (!$response) {
-         $response = new SS_HTTPResponse();
-      }
-      if ($page && $view && $view->RSSEnabled) {
+      if ($view) {
          $view->setTransientPaginationConfig($view->RSSItems, 'startItem');
-         $rss = $this->createFeed($view, $controller);
+         $rss = $this->createFeed($view, $controller, $response);
          $body = $rss->outputToBrowser();
          $response->setBody($body);
+         $response->addHeader('Content-Type', 'application/rss+xml');
          $response->setStatusCode(200);
-         $code = 200;
       } else {
          $response->setStatusCode(404);
       }
 
-      throw new SS_HTTPResponse_Exception($response);
-   }
-
-   /**
-    * This method is called by ContentController after it has initialized itself.
-    * We use it to intercept request handling if the action for the page is "rss".
-    */
-   public function onAfterInit() {
-      $controller = $this->owner;
-      $params = $controller->request->allParams();
-      if (array_key_exists('Action', $params) && $params['Action'] == 'rss' && array_key_exists('ID', $params)) {
-         $this->interceptRequest($controller, $params['ID']);
-      }
+      return $response;
    }
 
 }
