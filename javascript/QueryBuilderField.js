silvermine/silverstripe-views

commit 529cd0127724992040efb3b7200f6ebeca8656ca

Adds user-adjustable sorting to aggregated views
Adding this feature required reworking how some aspects of how the
QueryBuilderField worked. Instead of using QueryBuilderField to select views for
the ViewAggregatingResultsRetriever, we now use a GridField (similar to how
HandPickedResultsRetriever works) to select and sort views. Internally this user
an extra SortOrder column on the intersection table.

This change alloed QueryBuilderField to be simplified quite a bit since it no
longer had to support many_many relationships.

javascript/QueryBuilderField.js
@@ -547,8 +547,8 @@
                return;
             }
 
-            exportLink = importExport.find('.export a').get(0);
-            URL.revokeObjectURL(exportLink.href);
+            exportLink = importExport.find('.export a');
+            URL.revokeObjectURL(exportLink.attr('href'));
 
             blob = new Blob([json], {"type": "application\/json"});
             exportLink.href = URL.createObjectURL(blob);

code/AddPageToSortedManyManyAutocompleter.php
@@ -9,11 +9,18 @@
  * @package silverstripe-views
  * @subpackage code
  */
-class AddPageToHandPickedResultsRetrieverAutocompleter extends GridFieldAddExistingAutocompleter {
+class AddPageToSortedManyManyAutocompleter extends GridFieldAddExistingAutocompleter {
+   private $foreignColumn = '';
+   private $sortColumn = '';
 
-   public function __construct($targetFragment = 'before', $searchFields = null) {
+
+   public function __construct($foreignColumn, $sortColumn, $targetFragment = 'before', $searchFields = null) {
+      $this->foreignColumn = $foreignColumn;
+      $this->sortColumn = $sortColumn;
+
       parent::__construct($targetFragment, $searchFields);
    }
+
 
    /**
     * Add the page, but also fix the sort order.
@@ -32,8 +39,11 @@ class AddPageToHandPickedResultsRetrieverAutocompleter extends GridFieldAddExist
          // this results retriever so that a second addition would not
          // result in two pages having a zero value for SortOrder
          $sql = sprintf(
-            'UPDATE %s SET SortOrder = SortOrder + 1 WHERE HandPickedResultsRetrieverID = %d',
+            'UPDATE %s SET %s = %s + 1 WHERE %s = %d',
             $dataList->getJoinTable(),
+            $this->sortColumn,
+            $this->sortColumn,
+            $this->foreignColumn,
             $dataList->getForeignID()
          );
          DB::query($sql);

code/HandPickedResultsRetriever.php
@@ -119,9 +119,11 @@ class HandPickedResultsRetriever extends ViewResultsRetriever {
     */
    public function loadPreservedFields($data) {
       $pages = array_key_exists('Pages', $data) ? $data['Pages'] : array();
+      $i = 0;
       $this->Pages()->removeAll();
-      foreach($pages as $page)
-         $this->Pages()->add($page);
+      foreach ($pages as $page) {
+         $this->Pages()->add($page, array('SortOrder' => ++$i));
+      }
    }
 
    /**
@@ -175,26 +177,26 @@ class HandPickedResultsRetriever extends ViewResultsRetriever {
          ->removeComponentsByType('GridFieldDeleteAction')
          ->removeComponentsByType('GridFieldAddNewButton')
          ->removeComponentsByType('GridFieldAddExistingAutocompleter')
-         ->addComponent(new AddPageToHandPickedResultsRetrieverAutocompleter('buttons-before-left'))
+         ->addComponent(new AddPageToSortedManyManyAutocompleter('HandPickedResultsRetrieverID', 'SortOrder', 'buttons-before-left'))
          ->addComponent(GridFieldUpDownSortAction::create('SortOrder')->toTop())
          ->addComponent(GridFieldUpDownSortAction::create('SortOrder')->up())
          ->addComponent(GridFieldUpDownSortAction::create('SortOrder')->down())
          ->addComponent(GridFieldUpDownSortAction::create('SortOrder')->toBottom())
-         ->addComponent(new GridFieldDeleteAction($removeRelation = true))
-      ;
+         ->addComponent(new GridFieldDeleteAction($removeRelation = true));
+
       $autocompleter = $config->getComponentByType('GridFieldAddExistingAutocompleter');
       $autocompleter->setSearchList($this->createSearchDataList());
       $autocompleter->setResultsFormat($this->config()->get('autocomplete_format'));
       $autocompleter->setSearchFields($this->config()->get('autocomplete_search_fields'));
 
-      $picker = new GridField(
+      $fields->addFieldToTab('Root.QueryEditor', new GridField(
          'Pages',
-         _t('Views.HandPickedPagesLabel', 'Pages'),
+         _t('Views.HandPickedPages.Label', 'Pages'),
          $this->Pages(),
          $config
-      );
-      $fields->addFieldToTab('Root.Main', $picker);
+      ));
    }
+
 
    protected function createSearchDataList() {
       $list = DataList::create('SiteTree');

code/View.php
@@ -299,4 +299,9 @@ class View extends DataObject {
       $this->paginationURLParam = $paginationURLParam;
       return $this;
    }
+
+
+   public function Summary() {
+      return "{$this->Name} [{$this->getPage()->Title}]";
+   }
 }

code/ViewAggregatingResultsRetriever.php
@@ -19,49 +19,18 @@ class ViewAggregatingResultsRetriever extends ViewResultsRetriever {
       'Views' => 'View',
    );
 
-   static $has_one = array(
+   public static $many_many_extraFields = array(
+      'Views' => array(
+         'SortOrder' => 'Int',
+      ),
+   );
+
+   public static $has_one = array(
       'Sorter' => 'ViewResultsSorter',
    );
 
-   static $traverse_has_one = true;
+   public static $traverse_has_one = true;
 
-   /**
-    * Augment the QueryBuilderField type description so that it includes an
-    * entry for the View class. Includes a multiple-choice view choosing dropdown.
-    * Called by {@link QueryBuilderField::build_core_type_structures()}
-    *
-    * @param array &$structure
-    */
-   public static function augment_types(&$structure) {
-      $options = array();
-
-      // TODO: when doing aggregation, you should have the ability to sort child views
-      // instead of sorting them by name
-      // That will give you the ability to say "show all results from this view before
-      // any results from the other view" if you configure each view with its own sorting
-      // and don't configure a sorter.
-      $views = View::get()
-         ->innerJoin('ViewCollection', '"ViewCollection".ID = "View".ViewCollectionID')
-         ->innerJoin('SiteTree', '"SiteTree".ViewCollectionID = "ViewCollection".ID')
-         ->sort('"View".Name ASC')
-      ;
-
-      foreach ($views as $view)
-         $options[$view->ID] = "{$view->Name} &ndash; {$view->getPage()->Summary()}";
-
-      $viewStructure = array(
-         'base' => null,
-         'fields' => array(
-            'ID' => array(
-               'type' => 'select',
-               'default' => '',
-               'options' => $options
-            )
-         )
-      );
-
-      $structure['View'] = $viewStructure;
-   }
 
    /**
     * @see ViewResultsRetriever->count()
@@ -73,6 +42,22 @@ class ViewAggregatingResultsRetriever extends ViewResultsRetriever {
       }
       return $count;
    }
+
+
+   /**
+    * @see ViewResultsRetriever->dumpPreservedFields()
+    */
+   public function dumpPreservedFields() {
+      $views = array();
+      foreach($this->Views() as $view) {
+         $views[] = $view;
+      }
+
+      return array(
+         'Views' => $views
+      );
+   }
+
 
    /**
     * @see ViewResultsRetriever->getReadOnlySummary()
@@ -89,23 +74,18 @@ class ViewAggregatingResultsRetriever extends ViewResultsRetriever {
       return $html;
    }
 
-   /**
-    * Return an array representation of the Views relationship.
-    * Called by {@link QueryBuilderField::buildObjectStructure()}
-    *
-    * @return array
-    */
-   public function getViewsStructure() {
-      $viewIDs = array();
-      foreach ($this->Views() as $view) {
-         $viewIDs[] = array(
-            'type' => get_class($view),
-            'fields' => array('ID' => $view->ID)
-         );
-      }
 
-      return $viewIDs;
+   /**
+    * @see ViewResultsRetriever->loadPreservedFields()
+    */
+   public function loadPreservedFields($data) {
+      $views = array_key_exists('Views', $data) ? $data['Views'] : array();
+      $i = 0;
+      $this->Views()->removeAll();
+      foreach($views as $view)
+         $this->Views()->add($view, array('SortOrder' => ++$i));
    }
+
 
    /**
     * Deletes the associated many_many rows for hand-picked pages before
@@ -118,27 +98,7 @@ class ViewAggregatingResultsRetriever extends ViewResultsRetriever {
       parent::Views()->removeAll();
    }
 
-   /**
-    * Return the DataObject for a view defined in the given representation.
-    * Called by {@link QueryBuilderField::save_object()}
-    *
-    * Input is the same as the output of {@link ViewAggregatingResultsRetriever::getViewsStructure()}
-    *
-    * @param array
-    * @return View
-    */
-   public function resolveViewsStructure($view) {
-      $viewID = $view['fields']['ID'];
-      $view = View::get_one('View', 'ID = ' . Convert::raw2sql($viewID));
-      if (empty($view))
-         return;
 
-      return $view;
-   }
-
-   /**
-    * @see ViewResultsRetriever->resultsImpl()
-    */
    protected function resultsImpl() {
       $all = new ArrayList(array());
       foreach ($this->Views() as $view) {
@@ -156,14 +116,42 @@ class ViewAggregatingResultsRetriever extends ViewResultsRetriever {
       return $all;
    }
 
-   /**
-    * @see ViewResultsRetriever->updateCMSFields()
-    */
+
    public function updateCMSFields(&$view, &$fields) {
       parent::updateCMSFields($view, $fields);
+
+      $config = GridFieldConfig_RelationEditor::create($itemsPerPage = 20)
+         ->removeComponentsByType('GridFieldEditButton')
+         ->removeComponentsByType('GridFieldDeleteAction')
+         ->removeComponentsByType('GridFieldAddNewButton')
+         ->removeComponentsByType('GridFieldAddExistingAutocompleter')
+         ->addComponent(new AddPageToSortedManyManyAutocompleter('ViewAggregatingResultsRetrieverID', 'SortOrder', 'buttons-before-left'))
+         ->addComponent(GridFieldUpDownSortAction::create('SortOrder')->toTop())
+         ->addComponent(GridFieldUpDownSortAction::create('SortOrder')->up())
+         ->addComponent(GridFieldUpDownSortAction::create('SortOrder')->down())
+         ->addComponent(GridFieldUpDownSortAction::create('SortOrder')->toBottom())
+         ->addComponent(new GridFieldDeleteAction($removeRelation = true));
+
+      $autocompleter = $config->getComponentByType('GridFieldAddExistingAutocompleter');
+      $autocompleter->setSearchList(View::get());
+      $autocompleter->setResultsFormat('$Summary');
+      $autocompleter->setSearchFields(array('Name'));
+
+      $fields->addFieldToTab('Root.QueryEditor', new GridField(
+         'Views',
+         _t('Views.AggViews.Label', 'Views'),
+         $this->Views(),
+         $config
+      ));
    }
 
-   protected function shouldAddQueryBuilder() {
-      return true;
+
+   /**
+    * Sort the Views relationship correctly
+    *
+    * @return SS_List or null the pages associated with this results retriever
+    */
+   public function Views() {
+      return parent::Views()->sort('SortOrder ASC');
    }
 }

code/ViewResultsRetriever.php
@@ -97,14 +97,15 @@ class ViewResultsRetriever extends DataObject {
       }
    }
 
+
    /**
     * Used to load fields the need to be preserved by, but
     * not modified by QueryBuilderField upon a save action.
     *
     * @param array
     */
-   public function loadPreservedFields($data) {
-   }
+   public function loadPreservedFields($data) {}
+
 
    /**
     * Return the results
@@ -143,18 +144,7 @@ class ViewResultsRetriever extends DataObject {
     * @param FieldList the fields for this view form
     */
    public function updateCMSFields(&$view, &$fields) {
-      if ($this->shouldAddQueryBuilder()) {
-         $editor = new QueryBuilderField(
-            __CLASS__,
-            _t('Views.QueryBuilder.Label', 'QueryBuilder'),
-            $this
-         );
-
-         $fields->addFieldToTab('Root.QueryEditor', $editor);
-      }
-   }
-
-   protected function shouldAddQueryBuilder() {
-      return false;
+      $editor = new QueryBuilderField('ViewResultsRetriever', $this, $allowExport = false);
+      $fields->addFieldToTab('Root.QueryEditor', $editor);
    }
 }

code/query-results/QueryResultsRetriever.php
@@ -147,12 +147,23 @@ class QueryResultsRetriever extends ViewResultsRetriever {
       return $results;
    }
 
+
    public function Sorts() {
       return parent::Sorts()->sort('ID');
    }
 
-   protected function shouldAddQueryBuilder() {
-      return true;
+
+   /**
+    * All subclasses should implement this function, which provides them a way
+    * of adding fields to the "add/edit view" CMS form.  These fields will be
+    * what the user uses to modify this results retriever.
+    *
+    * @param View reference to the view that contains this results retriever
+    * @param FieldList the fields for this view form
+    */
+   public function updateCMSFields(&$view, &$fields) {
+      $editor = new QueryBuilderField('ViewResultsRetriever', $this);
+      $fields->addFieldToTab('Root.QueryEditor', $editor);
    }
 }
 

code/query-ui/QueryBuilderField.php
@@ -14,6 +14,8 @@ class QueryBuilderField extends FormField {
    protected $readonly = false;
    protected $disabled = false;
    private $resultsRetriever = null;
+   private $allowExport = true;
+
 
    /**
     * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
@@ -134,7 +136,6 @@ class QueryBuilderField extends FormField {
 
       $fields = array_merge($fields, QueryBuilderField::get_database_property($cls, 'has_one'));
       $fields = array_merge($fields, QueryBuilderField::get_database_property($cls, 'has_many'));
-      $fields = array_merge($fields, QueryBuilderField::get_database_property($cls, 'many_many'));
 
       return $fields;
    }
@@ -144,7 +145,7 @@ class QueryBuilderField extends FormField {
     * Given a class, return the fields matching the kind of relationship provided.
     *
     * @param class $cls DataObject Class
-    * @param string $prop [db, has_one, has_many, many_many]
+    * @param string $prop [db, has_one, has_many]
     * @return array('NameOfField' => 'FieldType')
     */
    public static function get_database_property($cls, $prop) {
@@ -153,10 +154,6 @@ class QueryBuilderField extends FormField {
       }
 
       if ($prop == 'has_many' && !QueryBuilderField::traverse_has_many_relationship($cls)) {
-         return array();
-      }
-
-      if ($prop == 'many_many' && !QueryBuilderField::traverse_many_many_relationship($cls)) {
          return array();
       }
 
@@ -382,8 +379,7 @@ class QueryBuilderField extends FormField {
          }
 
          $hasMany = QueryBuilderField::get_database_property($cls, 'has_many');
-         $manyMany = QueryBuilderField::get_database_property($cls, 'many_many');
-         foreach (array_merge($hasMany, $manyMany) as $property => $type) {
+         foreach ($hasMany as $property => $type) {
             $obj->$property()->removeAll();
 
             foreach (QueryBuilderField::get_value($fields, $property, array()) as $childStructure) {
@@ -435,34 +431,17 @@ class QueryBuilderField extends FormField {
 
 
    /**
-    * Return true if Object Representations for the given class
-    * should traverse it's many_many relationships
-    *
-    * @param string $cls
-    * @return bool
-    */
-   public static function traverse_many_many_relationship($cls) {
-      if (is_object($cls))
-         $cls = get_class($cls);
-
-      $relation = Config::inst()->get($cls, 'many_many');
-      return in_array($cls, array('ViewAggregatingResultsRetriever')) && !empty($relation);
-   }
-
-
-   /**
     * Object Constructor. Create a new form field.
     *
-    * @param string $name
-    * @param string $title
     * @param QueryResultsRetriever $resultsRetriever
-    * @param object $form Optional
     */
-   public function __construct($name, $title, ViewResultsRetriever $resultsRetriever, $form = null) {
+   public function __construct($name, ViewResultsRetriever $resultsRetriever, $allowExport = true) {
       $this->resultsRetriever = $resultsRetriever;
+      $this->allowExport = $allowExport;
+
       $structure = $this->buildQueryRepr($resultsRetriever);
 
-      parent::__construct($name, $title, json_encode($structure), $form);
+      parent::__construct($name, false, json_encode($structure));
    }
 
 
@@ -516,11 +495,6 @@ class QueryBuilderField extends FormField {
 
          // HasMany Relationships
          foreach (QueryBuilderField::get_database_property($cls, 'has_many') as $name => $type) {
-            $structure['fields'][$name] = $buildChildStructure($obj, $name);
-         }
-
-         // HasMany Relationships
-         foreach (QueryBuilderField::get_database_property($cls, 'many_many') as $name => $type) {
             $structure['fields'][$name] = $buildChildStructure($obj, $name);
          }
 
@@ -605,7 +579,11 @@ class QueryBuilderField extends FormField {
       }
 
       $html = "<div class='viewsQueryBuilder'></div>";
-      $html .= "<div class='viewsImportExport'></div>";
+
+      if ($this->allowExport) {
+         $html .= "<div class='viewsImportExport'></div>";
+      }
+
       $html .= $this->getInputTag();
       return $html;
    }

commit 52652388966ed1c3c5fa596d6e933ff93d3343f6

Fixes defect introduced in 529cd01277 which prevented view logic downloads from working

javascript/QueryBuilderField.js
@@ -564,8 +564,8 @@
             URL.revokeObjectURL(exportLink.attr('href'));
 
             blob = new Blob([json], {"type": "application\/json"});
-            exportLink.href = URL.createObjectURL(blob);
-            exportLink.download = "silverstripe-" + (new Date()).getTime() + ".view";
+            exportLink.attr('href', URL.createObjectURL(blob));
+            exportLink.attr('download', "silverstripe-" + (new Date()).getTime() + ".view");
          };
 
          $('input, select').live('change', save);